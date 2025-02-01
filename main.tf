ab9d3b48db3e9ea0422e63bd949c35d90bfaa065
mctrinity <mcgoodbyegirl24@gmail.com>
2025-02-01 17:37:03 +0800

    fix backedn variables on GitHub Actions variables

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = var.backend_rg_name
-    storage_account_name = var.backend_storage_account
-    container_name       = var.backend_container
-    key                  = var.backend_key
-  }
+  backend "azurerm" {}  # Leave this empty
 }
 
 provider "azurerm" {
